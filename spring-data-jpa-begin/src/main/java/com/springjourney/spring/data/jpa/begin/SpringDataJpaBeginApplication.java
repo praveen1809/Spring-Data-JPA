1b16a1efe779061ec5e5a2a52bb1e11260d79921
Praveen Kumar <praveenkumarbyreddi@gmail.com>
2022-03-16 22:41:11 +0530

    initial commit

---
+++ spring-data-jpa-begin/src/main/java/com/springjourney/spring/data/jpa/begin/SpringDataJpaBeginApplication.java
@@ -0,0 +1,13 @@
+package com.springjourney.spring.data.jpa.begin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaBeginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaBeginApplication.class, args);
+	}
+
+}
---
+++ spring-data-jpa-begin/src/test/java/com/springjourney/spring/data/jpa/begin/SpringDataJpaBeginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjourney.spring.data.jpa.begin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaBeginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
